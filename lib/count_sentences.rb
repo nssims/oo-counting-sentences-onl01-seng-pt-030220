1c4c7618f68a93e6c7345c71b6cbd91a09ff9b64
Nicole Sims <nssims105@gmail.com>
2020-04-04 00:52:40 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    new_self = self.split(/\S\?|\S\.|\S!/)
+    self_counted = new_self.count 
+    return self_counted
   end
+  
 end
